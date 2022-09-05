87cbfa2c032ad23900b4a0720789d53655f94cc2
Pedro Bueno <pedrolbb.e@gmail.com>
2022-09-04 22:43:36 -0300

    Removed Storage test

--- src/routes.ts
+++ src/routes.ts
@@ -1,20 +1,5 @@
-import { Router, Request, Response } from 'express'
-import multer from 'multer'
-import multerConfig from './services/storage/multer'
-import upload from './services/storage/firebase'
+import { Router } from 'express'
 
 const router = Router()
-
-// TODO: remove
-/** */
-router.post(
-  '/test',
-  [multer(multerConfig).single('file'), upload],
-  async (req: Request, res: Response): Promise<any> => {
-    console.log(req.file)
-    res.status(201).send()
-  }
-)
-/** */
 
 export { router }
