c93f4634cc9ed89e9c75d66341446dd5447689e3
Pedro Bueno <pedrolbb.e@gmail.com>
2022-09-13 17:37:55 -0300

    Added Email Change Controller

---
+++ src/controllers/ChangeEmailController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express'
+import userModel from '../models/userModel'
+import sendConfirmationCode from '../services/mail/sendConfirmationCode'
+
+class ChangeEmailController {
+  async handle(req: Request, res: Response): Promise<any> {
+    const { email } = req.body
+    const { user_id } = req
+
+    try {
+      await userModel.findByIdAndUpdate(user_id, {
+        $set: {
+          email,
+        },
+      })
+
+      await sendConfirmationCode(user_id, email)
+      res.send()
+    } catch (e) {
+      res.status(500).send({ error: true, message: 'Internal Server Error' })
+    }
+  }
+}
+
+export default ChangeEmailController
--- src/routes.ts
+++ src/routes.ts
@@ -3,12 +3,15 @@ import SignupController from './controllers/SignupController'
 import SigninController from './controllers/SigninController'
 import PasswordRecoveryController from './controllers/PasswordRecoveryController'
 import PasswordResetController from './controllers/PasswordResetController'
+import ChangeEmailController from './controllers/ChangeEmailController'
 
 const signupController = new SignupController()
 const signinController = new SigninController()
 const passwordRecoveryController = new PasswordRecoveryController()
 const passwordResetController = new PasswordResetController()
+const changeEmailController = new ChangeEmailController()
 
+import ensureAuthenticated from './middlewares/ensureAuthenticated'
 import Multer from './middlewares/Multer'
 
 const router = Router()
@@ -17,5 +20,6 @@ router.post('/signup', Multer.single('avatar'), signupController.handle)
 router.post('/signin', signinController.handle)
 router.post('/password_recovery', passwordRecoveryController.handle)
 router.post('/password_reset', passwordResetController.handle)
+router.post('/email_change', ensureAuthenticated, changeEmailController.handle)
 
 export default router
